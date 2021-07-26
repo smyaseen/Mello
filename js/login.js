bab9088b55a62eeec223d6e6482cb7d8e1085025
smyaseen <smyaseen164@gmail.com>
2021-07-26 00:10:24 -0700

    added dummy login

--- js/login.js
+++ js/login.js
@@ -1,16 +1,10 @@
 function login() {
-  console.log(document.getElementById("email").value);
-  console.log(document.getElementById("pass").value);
-
   if (
     document.getElementById("email").value === "admin@admin.com" &&
     document.getElementById("pass").value === "pass"
   ) {
-    (window.location.href = "../pages/mello.html"), true;
-    console.log("asd");
+    window.location.href = "../pages/mello.html";
   } else {
     alert("wrong username or pass");
   }
-
-  // return false;
 }
